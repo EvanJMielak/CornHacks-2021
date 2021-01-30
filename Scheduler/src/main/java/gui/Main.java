b882b80bdbdc1a2bdbd733d270c02df92dd6b237
MaxBielstein <78271520+MaxBielstein@users.noreply.github.com>
2021-01-30 13:36:33 -0600

    Reverted from javafx

--- Scheduler/src/main/java/gui/Main.java
+++ Scheduler/src/main/java/gui/Main.java
@@ -1,29 +1,8 @@
 package gui;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
 
-public class Main  extends Application{
+public class Main{
 	
 
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
-			Scene scene = new Scene(root, 1000, 700);
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Title");
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	
-	}
 
 }
